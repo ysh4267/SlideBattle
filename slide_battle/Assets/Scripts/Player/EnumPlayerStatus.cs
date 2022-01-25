eaef6a15f75d5758b3af8137491059d9688f4a18
woohyun95 <diddngus77@naver.com>
2022-01-25 10:11:45 +0900

    [충돌 처리] 캐릭터간 충돌 처리

---
+++ slide_battle/Assets/Scripts/Player/EnumPlayerStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumPlayerStatus 
+{
+    IDLE,
+    REFLECTED_BY_OBSTACLE,
+    COLLIDE_WITH_ENEMY,
+    MOVE
+}
